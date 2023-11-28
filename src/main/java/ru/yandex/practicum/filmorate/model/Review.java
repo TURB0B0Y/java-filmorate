9910b1b39779d3fc701cd1559cd3508714639d12
TURB0B0Y <stopmosk0@gmail.com>
2023-11-28 22:52:27 +0300

    review

--- src/main/java/ru/yandex/practicum/filmorate/model/Review.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Review.java
@@ -7,8 +7,6 @@ import lombok.ToString;
 
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Null;
-import javax.validation.constraints.Positive;
 
 @Getter
 @Setter
