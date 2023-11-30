12af5c18e3390753f4b29cbda3871afcaf9a5501
UvarovaJulia <uvarovater@yandex.ru>
2023-11-30 23:13:06 +0300

    добавлен функционал "Лента событий"

--- src/main/java/ru/yandex/practicum/filmorate/storage/FeedStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/FeedStorage.java
@@ -4,7 +4,6 @@ import ru.yandex.practicum.filmorate.enums.EventType;
 import ru.yandex.practicum.filmorate.enums.Operation;
 import ru.yandex.practicum.filmorate.model.Feed;
 
-import java.time.Instant;
 import java.util.Set;
 
 public interface FeedStorage {
